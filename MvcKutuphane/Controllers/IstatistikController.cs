675f53b9499668da86b60da21521321f84a55518
barisalpaslan <brsaslan333@gmail.com>
2024-04-23 11:00:04 +0300

    Vitrin kısmı tamamlandı.

---
+++ MvcKutuphane/Controllers/IstatistikController.cs
@@ -0,0 +1,20 @@
+﻿using MvcKutuphane.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphane.Controllers
+{
+    public class IstatistikController : Controller
+    {
+        // GET: Istatistik
+
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MvcKutuphane/Controllers/VitrinController.cs
+++ MvcKutuphane/Controllers/VitrinController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MvcKutuphane.Models.Entity;
+using MvcKutuphane.Models.Sınıflarım;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,25 @@ namespace MvcKutuphane.Controllers
     public class VitrinController : Controller
     {
         // GET: Vitrin
+
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            Class1 cs = new Class1();
+
+            cs.Deger1 = db.TBL_KITAP.ToList();
+            cs.Deger2 = db.TBL_HAKKIMIZDA.ToList();
+            //var degerler = db.TBL_KITAP.ToList();
+            return View(cs);
+        }
+        [HttpPost]
+        public ActionResult Index(TBL_ILETISIM t)
+        {
+            db.TBL_ILETISIM.Add(t);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
