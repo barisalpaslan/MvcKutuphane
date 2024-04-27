96d2cb42cfce652bce31fec424dc9ecde5121745
barisalpaslan <brsaslan333@gmail.com>
2024-04-27 09:59:21 +0300

    Öğrenci Paneli neredeyse tamamlandı.

--- MvcKutuphane/Controllers/OgrenciPanel/MesajController.cs
+++ MvcKutuphane/Controllers/OgrenciPanel/MesajController.cs
@@ -1,11 +1,43 @@
-﻿using System;
+﻿using MvcKutuphane.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace MvcKutuphane.Controllers.OgrenciPanel
 {
-    public class MesajController
+    public class MesajController : Controller
     {
+        // GET: Mesaj
+
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var uyemail = (string)Session["Mail"].ToString();
+            var mesajlar = db.TBL_MESAJLAR.Where(x =>x.ALICI == uyemail.ToString()).ToList();
+            return View(mesajlar);
+        }
+        [HttpGet]
+        public ActionResult YeniMesaj()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniMesaj(TBL_MESAJLAR t)
+        {
+            var uyemail = (string)Session["Mail"].ToString();
+            t.GONDEREN = uyemail.ToString();
+            t.TARIH = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.TBL_MESAJLAR.Add(t);
+            db.SaveChanges();
+            return RedirectToAction("Giden","Mesaj");
+        }
+        public ActionResult Giden()
+        {
+            var uyemail = (string)Session["Mail"].ToString();
+            var mesajlar = db.TBL_MESAJLAR.Where(x => x.GONDEREN == uyemail.ToString()).ToList();
+            return View(mesajlar);
+        }
     }
 }
---
+++ MvcKutuphane/Controllers/OgrenciPanel/OgrenciKitapController.cs
@@ -0,0 +1,22 @@
+﻿using MvcKutuphane.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphane.Controllers.OgrenciPanel
+{
+    public class OgrenciKitapController : Controller
+    {
+        // GET: OgrenciKitap
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var uyemail = (string)Session["Mail"];
+            var id = db.TBL_UYELER.Where(x => x.MAIL ==  uyemail.ToString()).Select(x => x.ID).FirstOrDefault();
+            var degerler = db.TBL_HAREKET.Where(x => x.ISLEMDURUM == true && x.UYE == id).ToList();
+            return View(degerler);
+        }
+    }
+}
